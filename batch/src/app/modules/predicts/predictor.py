2828294403c34fdaf4e66f2d0a4785827511dc87
pei223 <peidparc@gmail.com>
2025-03-20 13:56:50 +0900

    :sparkles: 推論時のメモリを推論に必要な情報だけにする & ログ見直し

--- batch/src/app/modules/predicts/predictor.py
+++ batch/src/app/modules/predicts/predictor.py
@@ -15,38 +15,26 @@ from structlog import get_logger
 from app.domain.articles import Article, ArticleWithFeature
 from app.domain.parameters import CAREFUL_LABELS, TOPICS
 from app.modules.predicts.utils import filter_by_candidates
+from app.modules.predicts.memory import SaveSwitchableChatMessageHistory
 
 _logger = get_logger()
 
 _topic_and_careful_label_input_template = f"""
 トピックの一覧を提示します。
 この後にニュース記事がどのトピックに合うか複数回質問するので長期間詳細に記憶してください。
-返信は不要なので""と出力してください。
 ---
 [{",".join(TOPICS + CAREFUL_LABELS)}]
 """
 
-_article_input_template = """
-このあと以下の記事について質問するので、詳細に記憶してください。
-返信は不要なので""と出力してください。
-タイトル: {title}
-キーワード: {keywords}
-本文: {body}
-"""
-
-_topic_prediction_input = f"""
+_topic_prediction_input = """
 記事の内容でトピックの中から一致するものを全て選んでください。最低1件は選んでください。
 選択肢にないものや疑わしいものは絶対に選ばないでください。
 
 回答はCSV形式で教えてください。CSV以外の文字は出力しないでください。
-"""
-
-_careful_label_prediction_input = f"""
-記事の内容でトピックBの中から一致するものを全て選んでください。
-選択肢にないものや疑わしいものは絶対に選ばないでください。
-
-回答はCSV形式で教えてください。CSV以外の文字は出力しないでください。
-一致するものがなければ空文字を出力してください。
+---
+タイトル: {title}
+キーワード: {keywords}
+本文: {body}
 """
 
 
@@ -57,59 +45,44 @@ class Predictor:
         os.environ["OPEN_API_KEY"] = open_ai_api_key
         model = ChatOpenAI(model="gpt-4o-mini")
         self._session_id = str(random.randint(0, 1000000))
-        history = InMemoryChatMessageHistory()
+        self._history = SaveSwitchableChatMessageHistory()
         self._memorize_chain = RunnableWithMessageHistory(
             model | StrOutputParser(),
-            lambda x: history,
+            lambda x: self._history,
         )
         self._topic_chain = RunnableWithMessageHistory(
             model | CommaSeparatedListOutputParser(),  # type: ignore
-            lambda x: history,
-        )
-        self._careful_label_chain = RunnableWithMessageHistory(
-            model | CommaSeparatedListOutputParser(),  # type: ignore
-            lambda x: history,
+            lambda x: self._history,
         )
         self._article_max_char_len_for_predict = article_max_char_len_for_predict
 
     def memorize_for_init(self):
+        self._history.save_memory_mode()
         memorize_for_init_res = self._memorize_chain.invoke(
             _topic_and_careful_label_input_template,
             config={"configurable": {"session_id": self._session_id}},
         )
         _logger.info("memorize for init res", res=memorize_for_init_res)
+        self._history.disable_save_memory_mode()
         return self
 
-    def memorize_article(self, article: Article) -> Self:
-        article_input = _article_input_template.format(
+    def predict(self, article: Article) -> ArticleWithFeature:
+        _logger.debug("debug output history", messages=self._history.messages)
+        input_for_prediction = _topic_prediction_input.format(
             title=article.title,
             body=article.body[: self._article_max_char_len_for_predict],
             keywords=",".join(article.keywords),
         )
-        memorize_article_res = self._memorize_chain.invoke(
-            article_input, config={"configurable": {"session_id": self._session_id}}
-        )
-        _logger.debug("memorize article res", res=memorize_article_res)
-        return self
-
-    def predict(self, article: Article) -> ArticleWithFeature:
         topic_res = self._topic_chain.invoke(
-            _topic_prediction_input,
+            input_for_prediction,
             config={"configurable": {"session_id": self._session_id}},
         )
-        if self._is_forget_topics(topic_res):
-            _logger.info("re-memorize for init", res=topic_res)
-            self.memorize_for_init()
-            topic_res = self._topic_chain.invoke(
-                _topic_prediction_input,
-                config={"configurable": {"session_id": self._session_id}},
-            )
 
-        if self._too_many_topics(topic_res):
-            # たまに大量にトピック選択された変な返答があるので1度だけやり直す
-            _logger.info("too many topics. retry", res=topic_res)
+        if self._is_topics_empty(topic_res) or self._too_many_topics(topic_res):
+            # たまに大量にトピック選択されたり空だったり変な返答があるので1度だけやり直す
+            _logger.info("too many topics or empty topics. retry", res=topic_res)
             topic_res = self._topic_chain.invoke(
-                _topic_prediction_input,
+                input_for_prediction,
                 config={"configurable": {"session_id": self._session_id}},
             )
 
@@ -120,13 +93,9 @@ class Predictor:
             careful_labels=filter_by_candidates(topic_res, CAREFUL_LABELS),
         )
 
-    def _is_forget_topics(self, res: list[str]) -> bool:
-        return (
-            "トピックが" in ".".join(res)
-            or "できません" in ".".join(res)
-            or "出来ません" in ".".join(res)
-        )
-
+    def _is_topics_empty(self, res: list[str]) -> bool:
+        return len(res) == 0
+    
     def _too_many_topics(self, res: list[str]) -> bool:
         # 閾値は経験則
         return len(res) > 20
